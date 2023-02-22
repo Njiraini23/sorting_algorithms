06d803ad0f926b1640876620855b6d8f3d72c544
Njiraini23 <njiraini88@gmail.com>
2023-02-22 10:58:06 +0000

    burble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- * bubble_sort -
- *
- * Return:
+ * bubble_sort - Function that sorts arrays
+ *@array: The array to be sorted
+ *@size: the size of array
+ * Return: The sorted arrays
  */
 void bubble_sort(int *array, size_t size)
 {
